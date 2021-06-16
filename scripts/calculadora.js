f21e7176d1279732d853733e327954882507f865
walmirlsj <walmir_004@hotmail.com>
2021-06-16 00:40:16 -0400

    Adicionando modularização js e css

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -1,76 +1,96 @@
+import { maquinaCalculadora } from './maquina.js';
 
-// document.getElementById("button").addEventListener("click", teste);
 var visorTemp = document.getElementById("visor-temp");
 var visor = document.getElementById("visor-input");
+var historico = document.getElementById("historico-list");
 document.getElementsByName("button").forEach(f => (f.addEventListener("click", pegaElementoValue)));
-// document.getElementById("visor-input").("keyup", preCalcular);
-document.addEventListener('keyup', processaTeclado);
-// document.getElementById("visor-input").addEventListener("keyDown", preCalcular);
-let statusFalha = false;
+document.addEventListener('keypress', processaTeclado);
+document.addEventListener('keydown', processaTecladoEspecial);
 
+var lastExp;
+
+let statusFalha = false;
+function processaTecladoEspecial(evt) {
+    if (evt.target.className != "visor-input") {
+        switch (evt.keyCode) {
+            case '8':
+                actionCalcular("CE");
+                break;
+            case 27:
+                visor.value = "";
+                visorTemp.value = "";
+                break;
+        }
+    }
+
+}
 function processaTeclado(keyEvent) {
-    // teste()
-    // 96 - 105 111 106 109 107 57 48 53 [0-9] / * - + ( ) %
-    // 110 108 . , // converte pra ponto
-    // 13 187 enter = funcao igual
-    const key = eval(keyEvent.keyCode);
+
+    const key = keyEvent.keyCode;
     const keyName = keyEvent.key;
-    keyEvent.preventDefault();
-    
-    console.log("keyCode: " + key)
-    if ((key > 95 && key <= 105)
-        || key == 111
-        || key == 106
-        || key == 109
-        || key == 107
-        || key == 57
-        || key == 48
-        || key == 53) {
-        teste(keyName);
-    } else if (key == 13 || key == 187) {
-        teste('=');
-    } else if (key == 108 || key == 110) {
-        teste('.');
-    } else if(key == 8){
-        teste("CE");
-    } else if(key == 27){
-        visor.value = "";
-        visorTemp.value = "";
+
+    try {
+        if (key == 47) {
+            actionCalcular('÷');
+        } else if ((key > 47 && key <= 57)
+            || key == 37
+            || key == 42 || key == 45
+            || key == 43 || key == 40
+            || key == 41
+        ) {
+            actionCalcular(keyName);
+        } else if (key == 13 || key == 43) {
+            actionCalcular('=');
+        } else if (key == 46 || key == 44) {
+            actionCalcular('.');
+        } else {
+            keyEvent.preventDefault();
+        }
+    } catch (error) {
+
     }
 }
 
 function pegaElementoValue(elementEvent) {
-    
-    teste(elementEvent.toElement.innerHTML);
+
+    actionCalcular(elementEvent.toElement.innerHTML);
 }
 
-function teste(entrada) {
-    //event1.toElement.innerHTML
-    // visor.value(event1.toElement.innerHTML);
-
+function actionCalcular(entrada) {
     var value = visor.value;
     switch (entrada) {
         case "CE":
             if (value.length > 0) {
                 visor.value = value.substring(0, value.length - 1);
-                preCalcular();
             }
             break;
         case "=":
-            // var temp = value;
             preCalcular();
             if (statusFalha) return false;
-            visor.value = calcular();
-            // visorTemp.innerHTML = "ans: " + temp;
+            
+            var temp = calcular();
+            if(lastExp == visor.value) return false;
+            lastExp = temp;
+            salvarExpressao(temp);
+            visor.value = temp;
+            
             break;
-        // case "":
         default:
             visor.value += entrada;
             preCalcular();
     }
-    
-}
 
+}
+function salvarExpressao(valor) {
+    // visor.value
+    
+    var element =  '<div style="display: inline-flex;">' +
+        '<span type="text" class="historico-temp" value="' + visor.value + '" style="width: 200px;" readonly>'+visor.value+'</span>' +
+        '<span type="text" class="historico-temp" value="' + valor + '" style="width: 90px;" readonly>'+valor+'</span>' +
+        '</div>';
+
+    historico.innerHTML = historico.getInnerHTML() + element;
+}
 
 function calcular() {
     var result = preCalcular();
@@ -81,7 +101,7 @@ function preCalcular() {
     var value = NaN;
     try {
         statusFalha = false;
-        value = calc(visor.value);
+        value = maquinaCalculadora(visor.value);
         visor.classList.remove("errorOperation")
     } catch (e) {
         console.log(e);
@@ -93,186 +113,3 @@ function preCalcular() {
     return value;
 }
 
-var stack = [];
-var fita = [];
-var p = 0;
-
-function calc(fita) {
-    // this.fita = fita;
-    fita = fita.replace('÷', '/');
-
-    this.fita = fita;
-    stack = [];
-    p = 0;
-    var calcula = false;
-
-    calcula = E();
-
-    var texto = "Exp " + fita + " valida: " + calcula;
-    if (p < fita.length && calcula) {
-        texto += " até a posicao: " + (p);
-        // console.log(texto);
-
-        throw new Error("texto");
-    }
-    // alert(texto);
-    var result = stack.pop();
-    return result;// 
-    // console.log(texto + " resultado " + stack.pop());
-    // console.log("---");
-    // return true;
-}
-
-function calcula(op) {
-    var v1 = stack.pop();
-    var v2 = stack.pop();
-    console.log(v1, v2, op);
-    var result = 0;
-    switch (op) {
-        case '*':
-            result = v2 * v1;
-            break;
-        case '/':
-            result = v2 / v1;
-            break;
-        case '+':
-            result = v2 + v1;
-            break;
-        case '-':
-            result = v2 - v1;
-            break;
-        case '%':
-            stack.push(v2);
-            result = v1 / 100;
-            break;
-        default:
-    }
-    // console.log(op)
-    // if (signal != '') result = 0 - result;
-    stack.push(result);
-
-}
-
-function E() {
-    if (T()) {
-        var pp = p;
-        if (match('+') || match('-')) {
-            if (!E()) return false;
-            calcula(fita[pp]);
-        }
-
-        return true;
-    }
-    return false;
-}
-
-function T() {
-    if (F()) {
-        var pp = p;
-        if (match('*') || match('/')) {
-            if (!T()) return false;
-            calcula(fita[pp]);
-        }
-        return true;
-    }
-    return false;
-}
-
-function F() {
-    if (match('-') && (fita[p] == '(' || p == 1) && F()) {
-        // corrige signal negativo 14/06/2021
-        stack.push(0 - stack.pop());
-        return true;
-    } else {
-        if (match('(')) {
-            E();
-            return match(')');;
-        }
-    }
-    var n = Num();
-    if (match('%')) {
-        calcula('%');
-    }
-    return n;
-}
-
-function Num() {
-    var s = 0;
-    var contadorDir = 0;
-    var valor = 0.0;
-    var valorDir = 0.0;
-    var erro = false;
-    var signal = 1.0;
-    var f = false; // float?
-
-    while (true) {
-        switch (s) {
-            case 0:
-                if (fita[p] >= '0' && fita[p] <= '9')
-                    s = 2;
-                else if (fita[p] == '+') s = 1;
-                else if (fita[p] == '-') {
-                    s = 1;
-                    signal = -1.0;
-                } else if (fita[p] == '.') {
-                    s = 3;
-                    f = true;
-                } else
-                    erro = true; //return false;
-                break;
-            case 1:
-                if (fita[p] >= '0' && fita[p] <= '9')
-                    s = 2;
-                else if (fita[p] == '.') {
-                    s = 3;
-                    f = true;
-                } else erro = true; //return false;
-                break;
-            case 2:
-                if (fita[p] >= '0' && fita[p] <= '9')
-                    s = 2;
-                else if (fita[p] == '.') {
-                    s = 3;
-                    f = true;
-                } else {
-                    stack.push(valor * signal);
-                    return true;
-                }
-                break;
-            case 3:
-                if (fita[p] >= '0' && fita[p] <= '9')
-                    s = 4;
-                else erro = true; //return false;;
-                break;
-            case 4:
-                if (fita[p] >= '0' && fita[p] <= '9') {
-                    s = 4;
-                } else {
-                    stack.push((valor + (valorDir / Math.pow(10, contadorDir))) * signal);
-                    return true;
-                }
-                break;
-        }
-        if (erro) {
-            throw new Error("Erro: Numero era esperado na posição: " + (p + 1) + " contem: '" + fita[p] + "'");
-        }
-        if (fita[p] != '-' && fita[p] != '+' && fita[p] != '.' && !f) {
-            valor *= 10;
-            valor += fita[p] - '0';
-        } else if (f && fita[p] != '.') {
-            contadorDir++;
-            valorDir *= 10;
-            valorDir += fita[p] - '0';
-        }
-        p++;
-    }
-}
-
-
-function match(valor) {
-    if (valor == fita[p]) {
-        p++;
-        return true;
-    }
-    return false;
-}
---
+++ scripts/maquina.js
@@ -0,0 +1,174 @@
+export { maquinaCalculadora };
+var stack = [];
+var fita = [];
+var p = 0;
+
+function maquinaCalculadora(fita1) {
+    fita = fita1.replace('÷', '/');
+
+    stack = [];
+    p = 0;
+    var calcula = false;
+
+    calcula = expressao();
+
+    var texto = "Exp " + fita + " valida: " + calcula;
+    if (p < fita.length && calcula) {
+        texto += " até a posicao: " + (p);
+        throw new Error(texto);
+    }
+    return stack.pop();
+}
+
+
+function calcula(op) {
+    var v1 = stack.pop();
+    var v2 = stack.pop();
+
+    var result = 0;
+    switch (op) {
+        case '*':
+            result = v2 * v1;
+            break;
+        case '/':
+            result = v2 / v1;
+            break;
+        case '+':
+            result = v2 + v1;
+            break;
+        case '-':
+            result = v2 - v1;
+            break;
+        case '%':
+            stack.push(v2);
+            result = v1 / 100;
+            break;
+        default:
+    }
+    stack.push(result);
+
+}
+
+function expressao() {
+    if (termo()) {
+        var pp = p;
+        if (match('+') || match('-')) {
+            if (!expressao()) return false;
+            calcula(fita[pp]);
+        }
+
+        return true;
+    }
+    return false;
+}
+
+function termo() {
+    if (fator()) {
+        var pp = p;
+        if (match('*') || match('/')) {
+            if (!termo()) return false;
+            calcula(fita[pp]);
+        }
+        return true;
+    }
+    return false;
+}
+
+function fator() {
+    if (match('-') && (fita[p] == '(' || p == 1) && fator()) {
+        // corrige signal negativo 14/06/2021
+        stack.push(0 - stack.pop());
+        return true;
+    } else {
+        if (match('(')) {
+            expressao();
+            return match(')');;
+        }
+    }
+    var n = numero();
+    if (match('%')) {
+        calcula('%');
+    }
+    return n;
+}
+
+function numero() {
+    var estado = 0;
+    var contadorDir = 0;
+    var valor = 0.0;
+    var valorDir = 0.0;
+    var erro = false;
+    var signal = 1.0;
+    var f = false; // float?
+
+    while (true) {
+        switch (estado) {
+            case 0:
+                if (fita[p] >= '0' && fita[p] <= '9')
+                    estado = 2;
+                else if (fita[p] == '+') estado = 1;
+                else if (fita[p] == '-') {
+                    estado = 1;
+                    signal = -1.0;
+                } else if (fita[p] == '.') {
+                    estado = 3;
+                    f = true;
+                } else
+                    erro = true;
+                break;
+            case 1:
+                if (fita[p] >= '0' && fita[p] <= '9')
+                    estado = 2;
+                else if (fita[p] == '.') {
+                    estado = 3;
+                    f = true;
+                } else erro = true;
+                break;
+            case 2:
+                if (fita[p] >= '0' && fita[p] <= '9')
+                    estado = 2;
+                else if (fita[p] == '.') {
+                    estado = 3;
+                    f = true;
+                } else {
+                    stack.push(valor * signal);
+                    return true;
+                }
+                break;
+            case 3:
+                if (fita[p] >= '0' && fita[p] <= '9')
+                    estado = 4;
+                else erro = true; //return false;;
+                break;
+            case 4:
+                if (fita[p] >= '0' && fita[p] <= '9') {
+                    estado = 4;
+                } else {
+                    stack.push((valor + (valorDir / Math.pow(10, contadorDir))) * signal);
+                    return true;
+                }
+                break;
+        }
+        if (erro) {
+            throw new Error("Erro: Numero era esperado na posição: " + (p + 1) + " contem: '" + fita[p] + "'");
+        }
+        if (fita[p] != '-' && fita[p] != '+' && fita[p] != '.' && !f) {
+            valor *= 10;
+            valor += fita[p] - '0';
+        } else if (f && fita[p] != '.') {
+            contadorDir++;
+            valorDir *= 10;
+            valorDir += fita[p] - '0';
+        }
+        p++;
+    }
+}
+
+
+function match(valor) {
+    if (valor == fita[p]) {
+        p++;
+        return true;
+    }
+    return false;
+}
